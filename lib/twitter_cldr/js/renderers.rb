703264f797afe8418d1ecb31ea1de4b343d41ba0
Arnav Kumar <arnav@arnavkumar.com>
2015-06-01 15:00:46 +0800

    Prune un-needed entries from compiler.rb and renderer.rb

--- lib/twitter_cldr/js/renderers.rb
+++ lib/twitter_cldr/js/renderers.rb
@@ -58,51 +58,17 @@ module TwitterCldr
         end
 
         module Shared
-          autoload :BidiRenderer,                         'twitter_cldr/js/renderers/data/shared/bidi_renderer'
           autoload :BreakIteratorRenderer,                'twitter_cldr/js/renderers/data/shared/break_iterator_renderer'
           autoload :CodePointRenderer,                    'twitter_cldr/js/renderers/data/shared/code_point_renderer'
           autoload :CalendarRenderer,                     'twitter_cldr/js/renderers/data/shared/calendar_renderer'
-          autoload :CurrenciesRenderer,                   'twitter_cldr/js/renderers/data/shared/currencies_renderer'
           autoload :LanguagesRenderer,                    'twitter_cldr/js/renderers/data/shared/languages_renderer'
           autoload :ListRenderer,                         'twitter_cldr/js/renderers/data/shared/list_renderer'
-          autoload :NumberingSystemsRenderer,             'twitter_cldr/js/renderers/data/shared/numbering_systems_renderer'
-          autoload :PhoneCodesRenderer,                   'twitter_cldr/js/renderers/data/shared/phone_codes_renderer'
-          autoload :PostalCodesRenderer,                  'twitter_cldr/js/renderers/data/shared/postal_codes_renderer'
-          autoload :LanguagesRenderer,                    'twitter_cldr/js/renderers/data/shared/languages_renderer'
-          autoload :UnicodeRegexRenderer,                 'twitter_cldr/js/renderers/data/shared/unicode_regex_renderer'
-          autoload :TerritoriesContainmentRenderer,       'twitter_cldr/js/renderers/data/shared/territories_containment_renderer'
         end
 
         module Parsers
           autoload :NumberParser,                         'twitter_cldr/js/renderers/data/parsers/number_parser'
-          autoload :SymbolTableRenderer,                  'twitter_cldr/js/renderers/data/parsers/symbol_table'
-          autoload :ComponentRenderer,                    'twitter_cldr/js/renderers/data/parsers/unicode_regex/component'
-          autoload :LiteralRenderer,                      'twitter_cldr/js/renderers/data/parsers/unicode_regex/literal'
-          autoload :UnicodeStringRenderer,                'twitter_cldr/js/renderers/data/parsers/unicode_regex/unicode_string'
-          autoload :CharacterClassRenderer,               'twitter_cldr/js/renderers/data/parsers/unicode_regex/character_class'
-          autoload :CharacterRangeRenderer,               'twitter_cldr/js/renderers/data/parsers/unicode_regex/character_range'
-          autoload :CharacterSetRenderer,                 'twitter_cldr/js/renderers/data/parsers/unicode_regex/character_set'
-          autoload :ParserRenderer,                       'twitter_cldr/js/renderers/data/parsers/parser'
-          autoload :SegmentationParserRenderer,           'twitter_cldr/js/renderers/data/parsers/segmentation_parser'
-          autoload :UnicodeRegexParserRenderer,           'twitter_cldr/js/renderers/data/parsers/unicode_regex_parser'
         end
 
-        module Tokenizers
-          autoload :TokenRenderer,                        'twitter_cldr/js/renderers/data/tokenizers/token'
-          autoload :CompositeTokenRenderer,               'twitter_cldr/js/renderers/data/tokenizers/composite_token'
-          autoload :TokenizerRenderer,                    'twitter_cldr/js/renderers/data/tokenizers/tokenizer'
-          autoload :SegmentationTokenizerRenderer,        'twitter_cldr/js/renderers/data/tokenizers/segmentation_tokenizer'
-          autoload :PatternTokenizerRenderer,             'twitter_cldr/js/renderers/data/tokenizers/pattern_tokenizer_renderer'
-          autoload :UnicodeRegexTokenizerRenderer,        'twitter_cldr/js/renderers/data/tokenizers/unicode_regex/unicode_regex_tokenizer'
-          autoload :RBNFTokenizerRenderer,                'twitter_cldr/js/renderers/data/tokenizers/numbers/rbnf_renderer'
-          autoload :NumberTokenizerRenderer,              'twitter_cldr/js/renderers/data/tokenizers/numbers/number_tokenizer_renderer'
-        end
-
-        module Utils
-          autoload :RangeRenderer,                        'twitter_cldr/js/renderers/data/utils/range'
-          autoload :RangeSetRenderer,                     'twitter_cldr/js/renderers/data/utils/range_set'
-          autoload :CodePointsRenderer,                   'twitter_cldr/js/renderers/data/utils/code_points'
-        end
       end
 
       module TestRenderers
